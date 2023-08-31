18f2720f8886b50e7f75ecc723a5b93b6f4f609e
Tamias <n.tamia5@icloud.com>
2023-08-31 13:11:01 +0300

    update 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,24 @@
-const name = "Miriam";
-const greeting = `Hello, my name is ${name}!`;
-console.log(greeting);
-// "Hello, my name is Miriam!"
+const startBtnEl = document.querySelector('[data-start]');
+const stopBtnEl = document.querySelector('[data-stop]');
+
+let timerId = null;
+stopBtnEl.disabled = true;
+
+startBtnEl.addEventListener('click', startColorSwitcher);
+stopBtnEl.addEventListener('click', stopColorSwitcher);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+function startColorSwitcher() {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+    startBtnEl.disabled = true;
+    stopBtnEl.disabled = false;
+}
+function stopColorSwitcher() {
+    clearInterval(timerId);
+    startBtnEl.disabled = false;
+    stopBtnEl.disabled = true;
+}

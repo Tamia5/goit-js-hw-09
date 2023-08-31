5e1bc486b5954a0458b009fda396eb1956c9743d
Tamia5 <47368598+Tamia5@users.noreply.github.com>
2023-08-31 10:11:27 +0000

    Deploying to gh-pages from @ Tamia5/goit-js-hw-09@18f2720f8886b50e7f75ecc723a5b93b6f4f609e 🚀

--- 01-color-switcher.e74c3d2d.js
+++
@@ -1,2 +0,0 @@
-!function(){var o="Hello, my name is ".concat("Miriam","!");console.log(o)}();
-//# sourceMappingURL=01-color-switcher.e74c3d2d.js.map
--- 01-color-switcher.fd348c5b.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello, my name is Miriam!");
-//# sourceMappingURL=01-color-switcher.fd348c5b.js.map
